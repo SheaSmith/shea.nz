eea3cce719430a46d2944641345782a5a2578bd7
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2020-09-18 15:15:16 +1200

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  @ViewChild('lightbox', { static: false })
+  lightbox: ElementRef;
+
+  currentImage = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  imageClick($event) {
+    const target = $event.target || $event.srcElement || $event.currentTarget;
+    this.currentImage = target.attributes.src.nodeValue;
+    
+    console.log(this.lightbox)
+    $(this.lightbox.nativeElement).modal();
+  }
+
+}
