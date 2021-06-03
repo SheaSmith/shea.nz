ab7e2149bfae4e70d2c7d568baabfbe7ed4d671c
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2021-06-03 23:27:48 +1200

    Custom cursor support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,105 @@ import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/cor
 export class AppComponent implements AfterViewChecked {
   title = 'PersonalWebsite';
 
+  cursors = {
+    'default': {
+      url: '/assets/cursors/arrow.png',
+      coords: '2 4'
+    },
+    'e-resize': {
+      url: '/assets/cursors/e-resize.png',
+      coords: '16 16'
+    },
+    'ew-resize': {
+      url: '/assets/cursors/e-resize.png',
+      coords: '16 16'
+    },
+    'help': {
+      url: '/assets/cursors/help.png',
+      coords: '2 4'
+    },
+    'move': {
+      url: '/assets/cursors/all-scroll.png',
+      coords: '16 16'
+    },
+    'all-scroll': {
+      url: '/assets/cursors/all-scroll.png',
+      coords: '16 16'
+    },
+    'context-menu': {
+      url: '/assets/cursors/arrow.png',
+      coords: '2 4'
+    },
+    'n-resize': {
+      url: '/assets/cursors/n-resize.png',
+      coords: '16 16'
+    },
+    'ne-resize': {
+      url: '/assets/cursors/ne-resize.png',
+      coords: '16 16'
+    },
+    'nesw-resize': {
+      url: '/assets/cursors/ne-resize.png',
+      coords: '16 16'
+    },
+    'ns-resize': {
+      url: '/assets/cursors/n-resize.png',
+      coords: '16 16'
+    },
+    'nw-resize': {
+      url: '/assets/cursors/nw-resize.png',
+      coords: '16 16'
+    },
+    'nwse-resize': {
+      url: '/assets/cursors/nw-resize.png',
+      coords: '16 16'
+    },
+    'no-drop': {
+      url: '/assets/cursors/no-drop.png',
+      coords: '16 16'
+    },
+    'not-allowed': {
+      url: '/assets/cursors/no-drop.png',
+      coords: '16 16'
+    },
+    'pointer': {
+      url: '/assets/cursors/link.png',
+      coords: '10 9'
+    },
+    'progress': {
+      url: '/assets/cursors/progress.png',
+      coords: '2 4'
+    },
+    's-resize': {
+      url: '/assets/cursors/n-resize.png',
+      coords: '16 16'
+    },
+    'se-resize': {
+      url: '/assets/cursors/nw-resize.png',
+      coords: '16 16'
+    },
+    'sw-resize': {
+      url: '/assets/cursors/ne-resize.png',
+      coords: '16 16'
+    },
+    'w-resize': {
+      url: '/assets/cursors/e-resize.png',
+      coords: '16 16'
+    },
+    'wait': {
+      url: '/assets/cursors/busy.png',
+      coords: '16 16'
+    }
+  }
+
   ngAfterViewChecked() {
-    // var elms = document.getElementsByTagName("*");
-    // var n = elms.length;
-    // for (var i = 0; i < n; i++) {
-    //   elms[i]['style'].cursor = `url(/assets/cursors/${window.getComputedStyle(elms[i]).cursor}.png), ${window.getComputedStyle(elms[i]).cursor}`;
-    // }
+    var elms = document.getElementsByTagName("*");
+    var n = elms.length;
+    for (var i = 0; i < n; i++) {
+      if (this.cursors[window.getComputedStyle(elms[i]).cursor] != null) {
+        const cursor = this.cursors[window.getComputedStyle(elms[i]).cursor];
+        elms[i]['style'].cursor = `url(${cursor.url}) ${cursor.coords}, ${window.getComputedStyle(elms[i]).cursor}`;
+      }
+    }
   }
 }
